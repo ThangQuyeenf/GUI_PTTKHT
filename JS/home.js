dbaad33ca0f007b8daa98a31161566a8f720d7e1
Wassefy <61963345+Wassefy@users.noreply.github.com>
2021-08-17 08:01:59 +0700

    Create home.js
    home page script

---
+++ JS/home.js
@@ -0,0 +1,16 @@
+var x = document.getElementById("stu_and_teach");
+var y = document.getElementById("department");
+var z = document.getElementById("btn");
+var ha = 6;
+function stu_and_teach(){
+    x.style.left = "50px";
+    y.style.left = "550px";
+    z.style.left = "0";
+}
+
+function department(){
+    x.style.left = "-500px";
+    y.style.left = "50px";
+    z.style.left = "180px";
+}
+
