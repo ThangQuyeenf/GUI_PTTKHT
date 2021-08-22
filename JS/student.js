c3d1944ee45d0d71adb7c51669ccdcbe1928b4e6
Wassefy <61963345+Wassefy@users.noreply.github.com>
2021-08-22 19:57:42 +0700

    Update student.js

--- JS/student.js
+++ JS/student.js
@@ -3,7 +3,6 @@ let container = document.querySelector('.container');
 let navigation = document.querySelector('.navigation');
 let navList = document.querySelector('.nav_list');
 let search = document.querySelector('.bx-search');
-let profile = document.querySelectorAll('.profile')
 menuToggle.onclick = function(e){
     container.classList.toggle('active');
     navigation.classList.toggle('active');
@@ -14,10 +13,37 @@ search.onclick = function(e){
     navigation.classList.toggle('active');
     navList.classList.toggle('active');
 }
+const function_list =[
+    'dashboard',
+    'profile'
+]
+// let profile = document.querySelectorAll('.profile');
+// let profileBtn = document.querySelector('.profile-btn');
+// profileBtn.onclick = function(){
+//     for(let i = 0 ;i< profile.length;i++){
+//         profile[i].classList.toggle('active');
+//     }
+// }
+// let dashboard = document.querySelector('.dashboard-btn')
+// dashboard.onclick = function(){
+//     for(let i = 0 ;i< profile.length;i++){
+//         profile[i].classList.toggle('active');
+//     }
+// }
 
-let profileBtn = document.querySelector('.bx-user-circle');
-profileBtn.onclick = function(){
-    for(let i = 0 ;i< profile.length;i++){
-        profile[i].classList.toggle('active');
+let list = document.querySelectorAll('.list');
+let content = document.querySelectorAll('.content');
+for( let i= 0; i<list.length; i++){
+    list[i].onclick = function(){
+        let j = 0;
+        while( j < list.length){
+            let tmp = j++;
+            list[tmp].className = 'list';
+            content[tmp].className = 'content';
+        }
+        list[i].className = 'list active';
+        content[i].className = 'content active';
     }
 }
+
+
